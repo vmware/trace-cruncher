487862abb8662b5ae4ab9dfea68ec71546d9671e
Yordan Karadzhov (VMware) <y.karadz@gmail.com>
2019-12-11 09:54:50 +0200

    trace-cruncher: Refactor the part of the interface that relies on libkshark
    This is the first patch from a patch-set that aims to refactor
    trace-cruncher completely. The goal it to be able to build the
    project as a native Python package, which contains several
    sub-packages implemented as C extensions via the Python's C API.
    In this patch the part of the interface that relies on libkshark
    gets re-implemented as an extension called "tracecruncher.ksharkpy".
    Note that this new extension has a stand-alone build that is
    completely decoupled from the existing build system used by
    trace-cruncher.
    
    Signed-off-by: Yordan Karadzhov (VMware) <y.karadz@gmail.com>

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+SPDX-License-Identifier: LGPL-2.1
+
+Copyright 2019 VMware Inc, Yordan Karadzhov (VMware) <y.karadz@gmail.com>
+"""
+
+from setuptools import setup, find_packages
+from distutils.core import Extension
+from Cython.Build import cythonize
+
+def main():
+    kshark_path = '/usr/local/lib/kernelshark'
+
+    module_ks = Extension('tracecruncher.ksharkpy',
+                          sources=['src/ksharkpy.c'],
+                          library_dirs=[kshark_path],
+                          runtime_library_dirs=[kshark_path],
+                          libraries=['kshark'],
+                          define_macros=[
+                              ('LIB_KSHARK_PATH', '\"' + kshark_path + '/libkshark.so\"'),
+                              ('KS_PLUGIN_DIR',   '\"' + kshark_path + '/plugins\"')
+                              ],
+                          )
+
+    setup(name='tracecruncher',
+          version='0.1.0',
+          description='NumPy based interface for accessing tracing data in Python.',
+          author='Yordan Karadzhov (VMware)',
+          author_email='y.karadz@gmail.com',
+          url='https://github.com/vmware/trace-cruncher',
+          license='LGPL-2.1',
+          packages=find_packages(),
+          ext_modules=[module_ks],
+          classifiers=[
+              'Development Status :: 3 - Alpha',
+              'Programming Language :: Python :: 3',
+              ]
+          )
+
+if __name__ == '__main__':
+    main()
